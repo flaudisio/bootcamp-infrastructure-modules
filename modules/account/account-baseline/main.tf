9411cbdda96a996a660a729a0b0e0a1844368f41
Flaudísio Tolentino <flaudisio@gmail.com>
2023-01-15 21:51:20 -0300

    feat(account/account-baseline): configure S3 public access block

--- modules/account/account-baseline/main.tf
+++ modules/account/account-baseline/main.tf
@@ -5,3 +5,14 @@
 resource "aws_iam_account_alias" "this" {
   account_alias = var.account_name
 }
+
+# ------------------------------------------------------------------------------
+# S3 ACCOUNT PUBLIC ACCESS BLOCK
+# ------------------------------------------------------------------------------
+
+resource "aws_s3_account_public_access_block" "this" {
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
